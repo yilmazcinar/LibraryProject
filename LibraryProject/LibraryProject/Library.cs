72beb21fa16dd872cb55c997cc698c7b400b55c7
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-15 19:34:54 +0300

    Add files via upload

---
+++ LibraryProject/LibraryProject/Library.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    
+
+    internal class Library // Kütüphane adında bir Class oluşturduk. Ve bu classtan kitap nesneleri oluşturacağız. 
+    {
+
+        public Library() // Parametre almayan default Constructor metodumuz. 
+        {
+            
+            KayitTarihi = DateTime.Now;
+            Ad = "Belirtilmedi";
+            YazarAd = "Belirtilmedi";
+            YazarSoyad = "Belirtilmedi";
+            SayfaSayisi = 0;
+            YayinEvi = "Belirtilmedi";
+
+
+
+
+
+        }
+
+        //Alınan bilgileri propertylere atayan Constructor metodumuz. 
+        public Library(string kitapAd, string yazarAd, string yazarSoyAd, int sayfaSayisi, string yayinEvi)
+        {
+            KayitTarihi = DateTime.Now;
+            Ad = kitapAd;
+            YazarAd = yazarAd;
+            YazarSoyad = yazarSoyAd;
+            SayfaSayisi = sayfaSayisi;
+            YayinEvi = yayinEvi;
+
+
+        }
+
+
+        // Propertiesler. Oluşturulan her nesne için özellikleri. 
+
+        public string Ad;
+
+        public string YazarAd;
+
+        public string YazarSoyad;
+
+        public int SayfaSayisi;
+
+        public string YayinEvi;
+
+        public DateTime KayitTarihi;
+
+    }
+}
---
+++ LibraryProject/LibraryProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using LibraryProject;
+
+Library kitap1 = new Library();
+
+Console.WriteLine($"Kitabın İsmi : {kitap1.Ad}\n\nYazar : {kitap1.YazarAd}\n\nSayfa Sayısı : {kitap1.SayfaSayisi}\n\nYayın Evi : {kitap1.YayinEvi}\n\nKayıt Tarihi : {kitap1.KayitTarihi}");
+
+
+Console.WriteLine("-----------------------------------------------------------");
+
+
+Library kitap2 = new Library("Yüzüklerin Efendisi", "J.R.", "Tolkien", 1900, "Keşan Kitap Evi");
+
+Console.WriteLine($"Kitabın İsmi : {kitap2.Ad}\n\nYazar : {kitap2.YazarAd} {kitap2.YazarSoyad}\n\nSayfa Sayısı : {kitap2.SayfaSayisi}\n\nYayın Evi : {kitap2.YayinEvi}\n\nKayıt Tarihi : {kitap2.KayitTarihi}");
+
+
